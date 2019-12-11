89723b94eed46b04a8a1d38e9077bd3d65eddade
cavallom <mlaracavallo@gmail.com>
2019-12-11 11:11:59 +0100

    Create a ZStack to show the wine & wineIcon images within the collection.

--- VivinoScreenSwiftUI/ContentView.swift
+++ VivinoScreenSwiftUI/ContentView.swift
@@ -27,15 +27,31 @@ struct ContentView: View {
                     
                     Spacer()
                     
-                    SearchView(txt: $txt).frame(width: 360, height: 60, alignment: .leading)
+                    SearchView(txt: $txt, data: data).frame(width: 360, height: 60, alignment: .leading)
                     .offset(x: 0, y: -40)
                     .padding(.bottom, -40)
+                    .zIndex(1)
                     
                     ScrollView {
                         HStack {
-                            Image("rocks").resizable().aspectRatio(contentMode: .fill).edgesIgnoringSafeArea(.top).frame(width: 160, height: 160)
-                            .padding()
-                            .border(Color.green, width: 2)
+                            ZStack {
+                                Image("winePic").resizable().aspectRatio(contentMode: .fill).edgesIgnoringSafeArea(.top).frame(width: 190, height: 190)
+                                .clipped()
+                                .blendMode(.screen)
+                                //.padding()
+                                .border(Color.green, width: 2)
+                                
+                                Image("wineIcon").renderingMode(.template)
+                                    .resizable().aspectRatio(contentMode: .fill).edgesIgnoringSafeArea(.top).frame(width: 60, height: 60)
+                                    .foregroundColor(Color.white)
+
+                                //.padding()
+                                
+                            }
+                            
+//                            Image("rocks").resizable().aspectRatio(contentMode: .fill).edgesIgnoringSafeArea(.top).frame(width: 160, height: 160)
+//                            .padding()
+//                            .border(Color.green, width: 2)
                             Image("rocks").resizable().aspectRatio(contentMode: .fill).edgesIgnoringSafeArea(.top).frame(width: 160, height: 160)
                             .padding()
                             .border(Color.green, width: 2)
@@ -99,6 +115,7 @@ struct ContentView_Previews: PreviewProvider {
 struct SearchView : View {
     
     @Binding var txt : String
+    var data : [String]
     
     var body : some View {
         VStack {
@@ -118,7 +135,16 @@ struct SearchView : View {
                     }.foregroundColor(.black)
                 }.padding()
             }
-        }
+//            List(self.data.filter{$0.lowercased().contains(self.txt.lowercased())}, id: \.self) { i in
+//                Text(i)
+//                
+//                
+//            }.frame(height: 60)
+            
+            
+            //Spacer()
+            
+        }.padding()
     }
 }
 
