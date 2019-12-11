01128ddee1379dee79891d47588364d981fed8cf
cavallom <mlaracavallo@gmail.com>
2019-12-11 19:55:49 +0100

    Encapuslate the whole view inside the ScrollView. Remove One WineZStack, add Vertical Stack and text views below the WineZStacks.

--- VivinoScreenSwiftUI/ContentView.swift
+++ VivinoScreenSwiftUI/ContentView.swift
@@ -19,44 +19,47 @@ struct ContentView: View {
         TabView {
             NavigationView {
                 
-                VStack {
-                    Image("barrels").resizable()
-                        .blendMode(.plusDarker)
-                        .aspectRatio(contentMode: .fill)
-                        .edgesIgnoringSafeArea(.top)
-                        .frame(height: 140)
-                        //            .scaledToFill()
-                        .clipped()
+                
+                ScrollView {
                     
-                    Spacer()
                     
-                    SearchView(txt: $txt, data: data).frame(width: 360, height: 60, alignment: .leading)
-                        .offset(x: 0, y: -40)
-                        .padding(.bottom, -40)
-                        .zIndex(1)
                     
                     VStack {
-                        Text("Browse wines")
-                            .font(.custom("Helvetica", size: 16))
-                            .fontWeight(.semibold).foregroundColor(Color("selectedTitle"))
-                        .padding()
+                        Image("barrels").resizable()
+                            .blendMode(.plusDarker)
+                            .aspectRatio(contentMode: .fill)
+                            .edgesIgnoringSafeArea(.top)
+                            .frame(height: 140)
+                            //            .scaledToFill()
+                            .clipped()
                         
-                        Picker(selection: $favoriteColor, label: Text("What is your favorite color?")) {
-                                        Text("").tag(0)
-                                       Text("").tag(1)
-                                       Text("").tag(2)
-                                   }.pickerStyle(SegmentedPickerStyle())
-                            .background(Color.white)
-                            .frame(width: 300, height: 10, alignment: .leading)
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
-                        //.foreground(Color.wineColor)
-
-                                   //Text("Value: \(favoriteColor)")
-                       
+                        Spacer()
+                        
+                        SearchView(txt: $txt, data: data).frame(width: 360, height: 60, alignment: .leading)
+                            .offset(x: 0, y: -40)
+                            .padding(.bottom, -40)
+                            .zIndex(1)
+                        
+                        VStack {
+                            Text("Browse wines")
+                                .font(.custom("Helvetica", size: 16))
+                                .fontWeight(.semibold).foregroundColor(Color("selectedTitle"))
+                                .padding()
                             
-                    }
-                    
-                    ScrollView {
+                            Picker(selection: $favoriteColor, label: Text("What is your favorite color?")) {
+                                Text("").tag(0)
+                                Text("").tag(1)
+                                Text("").tag(2)
+                            }.pickerStyle(SegmentedPickerStyle())
+                                .background(Color.white)
+                                .frame(width: 300, height: 10, alignment: .leading)
+                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
+                            //.foreground(Color.wineColor)
+                            
+                            //Text("Value: \(favoriteColor)")
+                            
+                            
+                        }
                         HStack {
                             WineZStack()
                             WineZStack()
@@ -69,35 +72,45 @@ struct ContentView: View {
                             
                         }
                         HStack {
-                            WineZStack()
-                            WineZStack()
-                        }
-                        HStack {
-                            
                             WineZStack()
                             WineZStack()
                         }
                         
+                        VStack {
+                            Text("One").padding()
+                            Text("Two").padding()
+                            
+                            Rectangle().frame(width: 330, height: 20, alignment: .leading)
+                                .foregroundColor(Color.gray)
+                            
+                            Spacer()
+                            
+                            Text("").padding()
+                            Text("").padding()
+                            Text("").padding()
+                            Text("").padding()
+                        }
+                        
                     }
-                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)) //this is making the top image wider
+                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)) //this is making the top image wider
                     
                 }.background(Color("lightBackground"))
                     .edgesIgnoringSafeArea(.all)
                     
-                .navigationBarItems(trailing: HStack {
-                    Button(action: {}) {
-                        Image(systemName: "cart.fill")
-                            .font(.system(size: 20))
-                    }.foregroundColor(Color.white)
-                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 20))
-                    
-                    Button(action: {}) {
-                        Image(systemName: "bell.fill")
-                            .font(.system(size: 20))
-                    }.foregroundColor(Color.white)
-                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 10))
-                    
-                })
+                    .navigationBarItems(trailing: HStack {
+                        Button(action: {}) {
+                            Image(systemName: "cart.fill")
+                                .font(.system(size: 20))
+                        }.foregroundColor(Color.white)
+                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 20))
+                        
+                        Button(action: {}) {
+                            Image(systemName: "bell.fill")
+                                .font(.system(size: 20))
+                        }.foregroundColor(Color.white)
+                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 10))
+                        
+                    })
                     .navigationBarTitle("", displayMode: .inline)
                     .font(.custom("Helvetica", size: 16))// this modifies the searchbar text
                 
@@ -123,12 +136,12 @@ struct SearchView : View {
             ZStack {
                 HStack {
                     TextField("Search", text: $txt).padding(.trailing, 75)
-                    .frame(height: 10)
+                        .frame(height: 10)
                 }.padding()
-//                    .background(Color.green)
-                     .background(Color.white)
+                    //                    .background(Color.green)
+                    .background(Color.white)
                     .cornerRadius(28)
-                .shadow(radius: 5)
+                    .shadow(radius: 5)
                 //.border(Color.red, width: 4)
                 
                 HStack {
